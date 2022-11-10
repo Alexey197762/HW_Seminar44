9bc6af3de93e84fadcf08a4832ca40a6d008a130
Алексей <pirat6666@gmail.com>
2022-11-10 22:41:14 +0300

    Задача 27

---
+++ Task_25/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит
+ число A в натуральную степень B. Для решения задания использование цикла for является 
+ обязательным условием. Не использовать встроенный метод возведения в степень.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int numberA = ReadInt32("Введите число A: ");
+int numberB = ReadInt32("Введите число B: ");
+degree(numberA, numberB);
+
+void degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt32(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Task_27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+Для решения задания использование цикла for является обязательным условием.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.WriteLine("Введите чесло");
+int namber = int.Parse(Console.ReadLine());
+int n = namber;
+int len = 0;
+int sum = 0;
+while(namber >0)
+{
+    len++;
+    namber = namber /10;
+}
+
+for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+        
+    }
+    Console.WriteLine($"сумма цифр {sum}");
